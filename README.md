777ef40cd70c57a7e2e48c697190668d4aa243b0
mfo <mfo@ordbogen.com>
2019-02-18 10:20:55 +0200

    adding read to read me + adjusting the API call

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 ## Weather App
 this with a play with with react to create a simple weather app
 
+created with `create-react-app` just clone the `yarn install` then `yarn-start`
+
 
--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
 
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
         const response = await api_call.json();
 
         // updating the initial stats of our app
